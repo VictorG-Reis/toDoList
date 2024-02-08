732e254e1b1e37e9a667fb6db270d90453683c42
victor gabriel R <victorgrs11@outlook.com>
2024-02-08 15:35:56 -0300

    (feat) putting inputs in login pagee

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,41 @@
+import { useState } from "react"
+
 function Login() {
+  
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('') 
+
+
+  const handleChange = ({target}) => {
+    target.name === 'email' ? setEmail(target.value) : setPassword(target.value) 
+    console.log(email,password);
+    
+    
+  }
+
   return(
     <>
       <h1>pagina de Login</h1>
+
+    <form action="submit">
+      <p>
+        <input
+        type="text"
+        name="email"
+        id="email" 
+        onChange={handleChange}/>
+      </p>
+
+      <p>
+        <input 
+        type="password" 
+        name="password" 
+        id="password"
+        onChange={handleChange}
+        />
+      </p>
+      <button>Entrar</button>
+    </form>
     </>
   )
 }
